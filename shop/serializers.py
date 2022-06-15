651bd0535f524d61e1d1b518cff0ed536a96f588
Tati_MS <90469788+tatiana-sthl@users.noreply.github.com>
2022-06-15 15:33:09 +0200

    modify serializers

--- shop/serializers.py
+++ shop/serializers.py
@@ -1,26 +1,38 @@
-from rest_framework.serializers import ModelSerializer
- 
-from shop.models import Category
-from shop.models import Product
-from shop.models import Article
+from rest_framework import serializers
 
- 
-class CategorySerializer(ModelSerializer):
- 
-    class Meta:
-        model = Category
-        fields = ['id', 'name']
+from shop.models import Category, Product, Article
 
 
-class ProductSerializer(ModelSerializer):
- 
-    class Meta:
-        model = Product
-        fields = ['id', 'date_created', 'date_updated', 'name', 'category']
+class ArticleSerializer(serializers.ModelSerializer):
 
-
-class ArticleSerializer(ModelSerializer):
- 
     class Meta:
         model = Article
         fields = ['id', 'date_created', 'date_updated', 'name', 'price', 'product']
+
+
+class ProductSerializer(serializers.ModelSerializer):
+
+    articles = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Product
+        fields = ['id', 'date_created', 'date_updated', 'name', 'category', 'articles']
+
+    def get_articles(self, instance):
+        queryset = instance.articles.filter(active=True)
+        serializer = ArticleSerializer(queryset, many=True)
+        return serializer.data
+
+
+class CategorySerializer(serializers.ModelSerializer):
+
+    products = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Category
+        fields = ['id', 'date_created', 'date_updated', 'name', 'products']
+
+    def get_products(self, instance):
+        queryset = instance.products.filter(active=True)
+        serializer = ProductSerializer(queryset, many=True)
+        return serializer.data
