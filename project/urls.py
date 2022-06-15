77a5722c7227d7bb143467166972c91cfd61882e
Tati_MS <90469788+tatiana-sthl@users.noreply.github.com>
2022-06-15 11:25:30 +0200

    add category and product

--- project/urls.py
+++ project/urls.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+ 
+from shop.views import CategoryAPIView
+from shop.views import ProductAPIView
 
+ 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls'))
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/category/', CategoryAPIView.as_view()),
+    path('api/product/', ProductAPIView.as_view())
+
 ]
---
+++ shop/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework.serializers import ModelSerializer
+ 
+from shop.models import Category
+from shop.models import Product
+
+ 
+class CategorySerializer(ModelSerializer):
+ 
+    class Meta:
+        model = Category
+        fields = ['id', 'name']
+
+
+class ProductSerializer(ModelSerializer):
+ 
+    class Meta:
+        model = Product
+        fields = ['id', 'date_created', 'date_updated', 'name', 'category']
